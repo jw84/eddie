3ca4e910b9bef153cb006787d6317ae0d47abd51
jw <jerrywang@me.com>
2013-12-18 13:37:59 -0800

    finished rest framework quickstart and now tutorial

--- nevergloomy/apps/myapp/urls.py
+++ nevergloomy/apps/myapp/urls.py
@@ -1,7 +1,14 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
 from apps.myapp import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# wire up our API using automatic url routing
+# additionally we include login urls for the browesable API
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^', include(router.urls)),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
