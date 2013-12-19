f72105dc3e86b85567b8c42c5b1f6f523baad004
jw <jerrywang@me.com>
2013-12-18 20:01:31 -0800

    adding relationships and hyperlinked apis

--- nevergloomy/apps/myapp/urls.py
+++ nevergloomy/apps/myapp/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from apps.myapp import views
+from apps.myapp.views import *
 
-urlpatterns = patterns('',
-	url(r'^snippets/$', views.SnippetList.as_view()),
-	url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-	url(r'^users/$', views.UserList.as_view()),
-	url(r'users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-)
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+# API endpoints
+urlpatterns = format_suffix_patterns(patterns('',
+	url(r'^$', 'api_root'),
+	url(r'^snippets/$', SnippetList.as_view(), name='snippet-list'),
+	url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view(), name='snippet-detail'),
+	url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view(), name='snippet-highlight'),
+	url(r'^users/$', UserList.as_view(), name='users-list'),
+	url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name='user-detail'),
+))
--- nevergloomy/apps/myapp/views.py
+++ nevergloomy/apps/myapp/views.py
@@ -3,6 +3,25 @@ from apps.myapp.serializers import SnippetSerializer
 from apps.myapp.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+
+@api_view(('GET',))
+def api_root(Request, format=None):
+	return Response({
+		'user': reverse('user-list', request=request, format=format),
+		'snippets': reverse('snippet-list', request=request, format=format)
+	})
+
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	renderer_classes = (renderers.StaticHTMLRenderer,)
+
+	def get(self, request, *Args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
 
 class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
